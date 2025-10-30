e99e4581913524bbcf79a61ccec78d56d9140ae8
thenameisatul <imatul024@gmail.com>
2025-10-30 16:01:13 +0530

    fix: update host and port for Render

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def add_user():
 
     return redirect('/')  # Refresh the page to show the new user
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
